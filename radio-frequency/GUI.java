f1a40d1fa27db2b1f97bf896632c9164b1e0282f
Jayed Rafi <jayedrafi@outlook.com>
2022-01-15 20:36:47 -0600

    Update GUI.java

--- radio-frequency/GUI.java
+++ radio-frequency/GUI.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class GUI extends JPanel {
 
@@ -24,14 +25,22 @@ public class GUI extends JPanel {
     private final int L_PADDING = 50; //Left padding for graph simulation frame
     private final int T_PADDING = 50; //Top padding for graph simulation frame
     private final int W_FRAME = 1100; // Length of graph simulation frame
-    private final int H_FRAME = 350; // Length of graph simulation frame
-    private static final int B_WIDTH = 200; //Button width
+    private final int H_FRAME = 350; // Height of graph simulation frame
+    private static final int B_WIDTH = 150; //Button width
     private static final int B_HEIGHT = 40; //Button height
     private static final int B_XPADDING = (WIDTH-50-1100-B_WIDTH)/2;//Button x-padding; (WIDTH-L_PADDING-W_FRAME-B-WIDTH)/2
     private static final int B_TPADDING = 50;//Button Y-padding
     private static final int B_INTERVAL = (350-(4*B_HEIGHT))/6;//Button in-between intervals
-    private final int Y_GRID = 35;
-    private final int X_GRID = 110;
+    private final int Y_GRID = 35;//Num of horizontal grid
+    private final int X_GRID = 110;//num of vertical grid
+    private final int POINT_INDICATOR_LENGTH = 5;
+
+    //Simulation value range
+    DecimalFormat dFormat = new DecimalFormat("#.##");//Format up to 2 decimal
+    private Double xMin = 0.0;
+    private Double yMin = -1312.22;
+    private Double xMax = 1000.0;
+    private Double yMax =1000.0;
 
     //Containers
     private List<Double>inValue; //Received value
@@ -53,25 +62,56 @@ public class GUI extends JPanel {
         //Y-grid
         int xIn, yIn, xFin, yFin; //x & y coordinates
         int gridInterval;
+        final int Y_POINT_INTERVAL = 5;
+        String point;
+        Double perSqr = (Math.abs(yMin)+Math.abs(yMax))/Y_GRID;//Value per square
         xIn = WIDTH-L_PADDING-W_FRAME;
         gridInterval = H_FRAME/Y_GRID;
         xFin = xIn+W_FRAME;
-        g2D.setColor(GRID_COLOR);
-        for(int i=0; i<Y_GRID; i++){
+
+        for(int i=0; i<Y_GRID+1; i++){
             yIn = yFin = T_PADDING+i*gridInterval;
-            g2D.drawLine(xIn, yIn, xFin, yFin);
+            g2D.setColor(GRID_COLOR);
+            g2D.drawLine(xIn, yIn, xFin, yFin);//grid
+            point = dFormat.format(yMax-i*perSqr)+"";
+            FontMetrics metrics = g2D.getFontMetrics();
+            int labelWidth = metrics.stringWidth(point);
+            if(i%Y_POINT_INTERVAL==0){
+                g2D.setColor(Color.BLACK);
+                g2D.drawString(point, xIn-labelWidth-4, yIn-4+metrics.getHeight()/2);//Point
+                g2D.setColor(Color.blue);
+                g2D.drawLine(xIn, yIn, xIn+POINT_INDICATOR_LENGTH, yFin);//Point indicator
+            }
         }
 
         //X-grid
         //Re-use Y-grid variables
+        final int X_POINT_INTERVAL = 10;
         yIn = T_PADDING;
         yFin=yIn+H_FRAME;
         gridInterval = W_FRAME/X_GRID;
-        for(int i=1; i<=X_GRID; i++){
+        for(int i=0; i<=X_GRID; i++){
             xIn = WIDTH-L_PADDING-W_FRAME+i*gridInterval;
             xFin = xIn;
+            g2D.setColor(GRID_COLOR);
             g2D.drawLine(xIn, yIn, xFin, yFin);
+            perSqr = (Math.abs(xMin)+Math.abs(xMax))/X_GRID;
+            point = dFormat.format(xMin+i*perSqr)+"";
+            FontMetrics metrics = g2D.getFontMetrics();
+            int labelWidth = metrics.stringWidth(point);
+            if(i%X_POINT_INTERVAL==0){
+                g2D.setColor(Color.BLACK);
+                g2D.drawString(point, xIn-labelWidth/2-2, T_PADDING+H_FRAME+metrics.getHeight()+2);//Point
+                g2D.setColor(Color.blue);
+                g2D.drawLine(xIn, yFin-POINT_INDICATOR_LENGTH, xFin, yFin);//Point indicator
+            }
+
         }
+
+        //Blue point indication line
+        g2D.setColor(Color.blue);
+        g2D.drawLine(WIDTH-L_PADDING-W_FRAME, T_PADDING+H_FRAME, WIDTH-L_PADDING-W_FRAME+W_FRAME,T_PADDING+H_FRAME);//y
+        g2D.drawLine(WIDTH-L_PADDING-W_FRAME,yIn,WIDTH-L_PADDING-W_FRAME,yFin);//x
 
     }//end simulateGraph
 

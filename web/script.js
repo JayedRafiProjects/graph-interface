a2d0d024bd253917a109c27095fd5e9c6f9a84e7
et-au <project.cmath@gmail.com>
2022-02-12 22:11:26 -0800

    Fixed on small screen

--- web/script.js
+++ web/script.js
@@ -1,9 +1,5 @@
-let shown = false
-
 document.querySelector('#navbar-toggler').addEventListener('click', (e) => {
   e.preventDefault()
   const nav = document.querySelector('#navbar-list')
-  if (!shown) nav.style.display = 'block'
-  else nav.style.display = 'none'
-  shown = !shown
+  nav.classList.toggle('hide')
 })

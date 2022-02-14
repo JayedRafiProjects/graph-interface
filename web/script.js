c4e0dc959d6cdff7f04669303eb5eba8fb0bad83
et-au <project.cmath@gmail.com>
2022-02-14 00:33:12 -0800

    Adding the original template, made responsive

--- web/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#navbar-toggler').addEventListener('click', (e) => {
-  e.preventDefault()
-  const nav = document.querySelector('#navbar-list')
-  nav.classList.toggle('hide')
-})
